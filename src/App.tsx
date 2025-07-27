7d85e48723b9e4f002ba8d153f08bde1f1e6d9b6
esckles <davidsamzzywilliams1234@gmail.com>
2025-07-27 18:10:41 +0100

    bg

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import imggg from "./assets/Screenshot_2025-07-14_123056-removebg-preview.png";
 
 const App = () => {
   return (
-    <div className="w-full min-h-screen bg-[#A0F1BD]">
+    <div className="w-full min-h-screen bg-blue-400">
       <header className="w-full h-[80px] border-b border-black flex items-center jusitfy-center">
         <div className="w-[99%] flex justify-between p-3 items-center">
-          <h4 className="text-[10px] lg:text-[30px]"> Noble Finances</h4>
+          <h4 className="text-[10px] lg:text-[30px]">FintFinance</h4>
           <div className="flex items-center justify-center gap-2">
             <p className="text-[8px] lg:text-[12px]">Services</p>
             <button className="bg-[#2E4F21] text-[10px] rounded-xl px-2 py-1 lg:px-4 lg:py-2 text-white ">
@@ -14,12 +14,12 @@ const App = () => {
           </div>
         </div>
       </header>
-      <div className="w-full flex justify-between p-4 h-[calc(100vh-80px)] items-center">
-        <div>
+      <div className="w-full flex justify-between p-4 h-[calc(100vh-80px)] items-center flex-col lg:flex  lg:flex-row">
+        <div className="mt-10">
           <h4 className="text-[30px] text-green-800">
             Financial Clarity You can Trust
           </h4>
-          <p className="text-[8px] font-semibold text-green-800 lg:text-[25px]">
+          <p className="text-[14px] font-semibold text-green-800 lg:text-[25px]">
             Trusted financial guidance for every stage of life and business
             since 1987
           </p>
@@ -27,11 +27,11 @@ const App = () => {
             Connect with our experts
           </button>
         </div>
-        <div>
+        <div className=" mb-10">
           <img
             src={imggg}
             alt=""
-            className="w-[200px] h-[170px] lg:w-[400px] lg:h-[400px]"
+            className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]"
           />
         </div>
       </div>
